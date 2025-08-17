9bf6a29f5d58a63f26a59b0a7e17e8a3351c1124
Syaifullah Abdul Fattah <105910658+syaif112233@users.noreply.github.com>
2025-08-18 02:23:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/dashboard-maahid/', // <— nama repo kamu
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
