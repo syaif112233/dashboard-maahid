a27af306421f6aa5cda98214db804190e34a9f5b
Syaifullah Abdul Fattah <105910658+syaif112233@users.noreply.github.com>
2025-08-18 01:03:07 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dashboard-sahabatku/', // <— nama repo kamu
+  base: '/dashboard-maahid/', // <— nama repo kamu
 })
